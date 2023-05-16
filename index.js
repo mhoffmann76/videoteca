f4b4dc551c33ac58c687a5db9dfafc8fa2eb9709
Marcelo Hoffmann <hoffmannmarcelo@hotmail.com>
2023-05-16 11:47:05 -0300

    Atualizado node.js para versão lts

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
 const database = require("./db/db");
 const express = require("express");
 const app = express();
-const Video = require("./models/videos");
-const VideoRoutes = require("./routes/routesvideos")
-const VideosControllers = require("./controllers/controllerVideo");
+const Video = require("./models/Video");
+const VideoRoutes = require("./routes/routesVideo");
 const hand = require("express-handlebars");
+
+const VideosControllers = require("./controllers/ControllerVideo");
 app.engine("handlebars", hand.engine());
 app.set("view engine", "handlebars");
-app.use(express.urlencoded({ extended: true }));
+
+app.use(express.urlencoded({extended: true,}));
 app.use(express.json());
-app.use(express.static("public"))
+app.use(express.static("public"));
 
-app.use("/", VideoHandler);
+app.use("/", VideoRoutes);
 
-
-
-
-try {
-  database.sync().then(() => {});
-
+database.sync().then(() => {
   app.listen(9443, () => {
     console.log("Servidor rodando na porta 9443");
   });
-} catch (erro) {
-  console.log(`Houve uma falha ao sincronizar com o banco de dados. ${erro}`);
-}
+  try {
+   
+  } catch (erro) {
+    console.log(`Houve uma falha ao sincronizar com o banco de dados. ${erro}`);
+  }
+});
