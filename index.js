b8b97a314c25a9af935b617d15f2df2154d05621
Marcelo Hoffmann <hoffmannmarcelo@hotmail.com>
2023-05-15 23:04:55 -0300

    Criado rotas

--- index.js
+++ index.js
@@ -2,6 +2,19 @@ const database = require("./db/db");
 const express = require("express");
 const app = express();
 const Video = require("./models/videos");
+const VideoRoutes = require("./routes/routesvideos")
+const VideosControllers = require("./controllers/controllerVideo");
+const hand = require("express-handlebars");
+app.engine("handlebars", hand.engine());
+app.set("view engine", "handlebars");
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"))
+
+app.use("/", VideoHandler);
+
+
+
 
 try {
   database.sync().then(() => {});
